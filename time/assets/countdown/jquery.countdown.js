6a85620146dcb92cd95ee1d70e7e1c92d013a693
Исиченко  Александр <shurik_i@ukr.net>
2019-01-25 16:07:56 +0200

    Update jquery.countdown.js

--- time/assets/countdown/jquery.countdown.js
+++ time/assets/countdown/jquery.countdown.js
@@ -1,10 +1,4 @@
-/**
- * @name		jQuery Countdown Plugin
- * @author		Martin Angelov
- * @version 	1.0
- * @url			http://tutorialzine.com/2011/12/countdown-jquery/
- * @license		MIT License
- */
+
 
 (function($){
 	
