3d2e2f1f665b07a41ed343038b96dcf730b2c3ba
Исиченко  Александр <shurik_i@ukr.net>
2020-06-17 10:27:53 +0300

    Upd

--- site-timer_new/js/countdown.js
+++ site-timer_new/js/countdown.js
@@ -30,7 +30,7 @@ CountdownTimer.prototype={
 function CDT(){
 
  // Set countdown limit
- var tl = new Date('2020/06/16 20:10:43');
+ var tl = new Date('2020/06/17 20:10:43');
 
  // You can add time's up message here
  var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end">Ураааа !</span></span>');
